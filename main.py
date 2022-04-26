b5e42a0b7bfde6b9fbb403c6c887e0721605f191
lamtell <jhormnjair21@gmail.com>
2022-04-25 22:51:00 -0500

    Se agregó Simulated Annealing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from QueenEnvironment import cal_heuristic
 from QueenEnvironment import size
 from primero_en_anchura.n_queens import NQueens
 import random
+import math
 import time
 from QueenEnvironment import State
 from QueenEnvironment import nCk
@@ -148,6 +149,70 @@ def reproduce(father, mother):
 
     return child
 
+class Node:
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        """Create a search tree Node, derived from a parent by an action."""
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
+
+    def expand(self, problem):
+        """List the nodes reachable in one step from this node."""
+        return [self.child_node(problem, action)
+                for action in problem.actions(self.state)]
+
+    def child_node(self, problem, action):
+        """[Figure 3.10]"""
+        next_state = problem.result(self.state, action)
+        #next_node = Node(next_state, self, action, problem.path_cost(self.path_cost, self.state, action, next_state))
+        next_node = Node(next_state, self, action)
+        return next_node
+# ______________________________________________________________________________
+
+''' USE OTHER SCHEDULE FUNCTION IF YOU WANT TO '''
+def schedule(t, k=20, lam=0.005, limit=1000):
+    """One possible schedule function for simulated annealing"""
+    return (k * math.exp(-lam * t) if t < limit else 0)
+
+
+''' WRITE THIS FUNCTION: '''
+
+def simulated_annealing(problem):
+    """See [Figure 4.5] for the algorithm."""
+    current = Node(problem.initial)         #create current tuple -1 * size
+    i=0
+    tiempo_de_ejecucion = []
+    suma_tiempo = 0
+    while i<1000000:                        #loop
+        inicio = default_timer()
+        t = schedule(i)
+        if (t==0 or -1 not in current.state):     #stop when t==0 or there is goal
+            axis_x = list(range(len(tiempo_de_ejecucion)))
+            axis_y = tiempo_de_ejecucion
+            title_axis_x = "Iteraciones"
+            title_axis_y = "Tiempo de Ejecución (ms)"
+            title = "Simulated Annealing"
+            number_iterations = str(len(tiempo_de_ejecucion))
+            return [current.state, axis_x, axis_y, title_axis_x, title_axis_y, title, number_iterations]                  #( -1 not in state)
+        if(current.expand(problem) != []):              #may explain
+            n = random.choice(current.expand(problem))      #random child
+            deltaE = problem.value(n) - problem.value(current)
+            if(deltaE<0):
+                print(1)
+            if(deltaE>0 or math.exp(deltaE // t) > 0.5):       #accept when ratio >0.5
+                current = n
+        else:                                           #cannot explain
+            current = Node(problem.initial)             #create new node
+            i=-1                               #back to the loop wit i=0, random again
+        i+=1                            #increase i
+        final = default_timer()
+        tiempo = (final - inicio) * 1000
+        suma_tiempo = suma_tiempo + tiempo
+        tiempo_de_ejecucion.append(suma_tiempo)
 
 def mutate(child):
     number_mutate = random.randint(0, size / 2)
@@ -232,6 +297,74 @@ class Genetic:
         return None
 
 
+class NQueensProblem:
+    """The problem of placing N queens on an NxN board with none attacking each other.
+    A state is represented as an N-element array, where a value of r in the c-th entry means there is a queen at column c,
+    row r, and a value of -1 means that the c-th column has not been filled in yet. We fill in columns left to right.
+
+    Sample code: iterative_deepening_search(NQueensProblem(8))
+    Result: <Node (0, 4, 7, 5, 2, 6, 1, 3)>
+    """
+
+    def __init__(self, N):
+        # self.initial = initial
+        self.initial = tuple([-1] * N)  # -1: no queen in that column
+        self.N = N
+
+    def board_resolve(self, size, vector):
+        matrix = []
+        for i in range(size):
+            matrix.append([0] * size)
+
+        for i in range(size):
+            for j in range(size):
+                if (vector[j] == i):
+                    matrix[i][j] = 1
+
+        for i in range(size):
+            for j in range(size):
+                print(matrix[i][j], end=" ")
+            print("")
+
+    def actions(self, state):
+        """In the leftmost empty column, try all non-conflicting rows."""
+        if state[-1] != -1:
+            return []  # All columns filled; no successors
+        else:
+            col = state.index(-1)
+            # return [(col, row) for row in range(self.N)
+            return [row for row in range(self.N)
+                    if not self.conflicted(state, row, col)]
+
+    def result(self, state, row):
+        """Place the next queen at the given row."""
+        col = state.index(-1)
+        new = list(state[:])
+        new[col] = row
+        return tuple(new)
+
+    def conflicted(self, state, row, col):
+        """Would placing a queen at (row, col) conflict with anything?"""
+        return any(self.conflict(row, col, state[c], c)
+                   for c in range(col))
+
+    def conflict(self, row1, col1, row2, col2):
+        """Would putting two queens in (row1, col1) and (row2, col2) conflict?"""
+        return (row1 == row2 or  # same row
+                col1 == col2 or  # same column
+                row1 - col1 == row2 - col2 or  # same \ diagonal
+                row1 + col1 == row2 + col2)  # same / diagonal
+
+    def value(self, node):
+        """Return (-) number of conflicting queens for a given node"""
+        num_conflicts = 0
+        for (r1, c1) in enumerate(node.state):
+            for (r2, c2) in enumerate(node.state):
+                if (r1, c1) != (r2, c2):
+                    num_conflicts += self.conflict(r1, c1, r2, c2)
+
+        return -num_conflicts
+
 def print_board(state):
     line = ''
     for i in range(size):
@@ -254,7 +387,7 @@ def print_board(state):
 
 
 def main():
-    bfs = False
+    bfs_or_sa = False
     env = QueenEnvironment.QueenEnvironment()
     solution = []
     agent = []
@@ -278,11 +411,14 @@ def main():
             print('BFS Solution %d:' % (i + 1))
             n_queens.print(solu)
         print('Total BFS solutions: %d' % len(bfs_solutions[0]))
-        bfs = True
+        bfs_or_sa = True
     elif choose == 2:
         agent = AStar(env.current_state, env.goal, env.action)
     elif choose == 3:
-        print("no se ha agregado nada")
+        problem1 = NQueensProblem(size)
+        solution = simulated_annealing(problem1)
+        problem1.board_resolve(size, solution[0])
+        bfs_or_sa = True
     elif choose == 4:
         state_list = []
         for i in range(20):
@@ -290,7 +426,7 @@ def main():
 
         agent = Genetic(state_list, 30)
 
-    if(bfs != True):
+    if(bfs_or_sa != True):
         solution = agent.search()
 
         state = solution[0]
